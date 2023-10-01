c3851d4432ec2ef43dc8df23c6efc929a3965a65
Iwo <g00391231@atu.ie>
2023-10-01 17:10:12 +0100

    Added Calculation class and added 'add' functionality

---
+++ src/main/java/ie/atu/calculator/Calculation.java
@@ -0,0 +1,22 @@
+package ie.atu.calculator;
+
+public class Calculation {
+    private int total;
+    private String operation;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
