4487b78d3fef5b0ab80d956537cb725ca4739c85
Iwo <g00391231@atu.ie>
2023-10-02 01:42:42 +0100

    Added multiplication functionality

--- src/main/java/ie/atu/calculator/CalculatorController.java
+++ src/main/java/ie/atu/calculator/CalculatorController.java
@@ -10,17 +10,23 @@ public class CalculatorController {
     @GetMapping("/calculate")
     public Calculation calculate(@RequestParam int a, @RequestParam int b, @RequestParam String operator) {
         Calculation calc = new Calculation();
-        if (operator.equals("add")) {
-            calc.setTotal(a + b);
-            calc.setOperation("add");
-        }
-        else if (operator.equals("subtract")) {
-            calc.setTotal(a - b);
-            calc.setOperation("subtract");
-        }
-        else if (operator.equals("multiply")) {
-            calc.setTotal(a * b);
-            calc.setOperation("multiply");
+        switch (operator) {
+            case "add":
+                calc.setTotal(a + b);
+                calc.setOperation("add");
+                break;
+            case "subtract":
+                calc.setTotal(a - b);
+                calc.setOperation("subtract");
+                break;
+            case "multiply":
+                calc.setTotal(a * b);
+                calc.setOperation("multiply");
+                break;
+            default:
+                calc.setOperation("NULL");
+                calc.setTotal(0);
+                break;
         }
         return calc;
     }
